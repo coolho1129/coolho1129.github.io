ce9afa1c83234f10bc9c2ad2bd5ac3eb076e5d44
CoolHo(김찬호) <coolho123@naver.com>
2023-12-27 13:40:36 +0900

    update changename.py

--- changename.py
+++ changename.py
@@ -1,18 +1,23 @@
 import os
 from natsort import natsorted
+import argparse
 
 def rename_files_in_folder(folder_path):
-    # 폴더 내의 파일 목록을 가져옴
     file_list =natsorted(os.listdir(folder_path))
-    print(file_list)
-
+    #print(file_list)
+    print()
     for i,filename in enumerate(file_list):
         old_filepath = os.path.join(folder_path, filename)
         new_filename = 'img'+str(i+1)+'.png'
         new_filepath = os.path.join(folder_path, new_filename)
+        new_filepath=new_filepath.replace("\\", "/")
         os.rename(old_filepath, new_filepath)
+        link = f"![{new_filename}]({{site.url}}/{new_filepath})" + "{: .align-center}"
+        print(link)
 
 if __name__ == "__main__":
-   
-    folder_path = "images\MLRecog"  
+    parser = argparse.ArgumentParser(description = '이미지 이름을 순서대로 img.png로 바꾸는 스크립트')
+    parser.add_argument('-src','-s', type = str, help = '이미지 파일 경로를 입력해주세요.')
+    args = parser.parse_args()
+    folder_path = args.src
     rename_files_in_folder(folder_path)
